bc9fb20da6504ec07f9c531084cd57e416930f8a
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-03-17 12:07:51 +0200

    Add POST createUser

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 const http = require("http");
 const fs = require("fs");
 const PORT = 3000;
+const users = [];
+
+const routeGET = {
+  "/": "./views/index.html",
+  "/about": "./views/about.html",
+  "/contacts": "./views/contacts.html",
+};
 
 const requestListener = (req, res) => {
   console.log("req", req.method, req.url);
   // console.log("res", res);
   const { method, url } = req;
   if (method === "GET") {
-    if (url === "/") {
-      fs.readFile("./views/index.html", { encoding: "utf-8" }, (err, data) => {
-        if (err) {
-          console.log(err);
-        }
-        res.end(data);
+    fs.readFile(routeGET[url], { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.end(data);
+    });
+    return;
+  }
+  if (method === "POST") {
+    if (url === "/create-user") {
+      let jsonString = "";
+      req.on("data", (chunk) => {
+        jsonString += chunk;
       });
-      return;
-    }
-    if (url === "/about") {
-      fs.readFile("./views/about.html", { encoding: "utf-8" }, (err, data) => {
-        if (err) {
-          console.log(err);
-        }
-        res.end(data);
-      });
-      return;
-    }
-    if (url === "/contacts") {
-      fs.readFile("./views/contacts.html", { encoding: "utf-8" }, (err, data) => {
-        if (err) {
-          console.log(err);
-        }
-        res.end(data);
+      req.on("end", () => {
+        const user = JSON.parse(jsonString); //object
+        delete user.password;
+        users.push(user);
+        res.end(JSON.stringify(user));
       });
       return;
     }
